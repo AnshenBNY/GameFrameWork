7f675aac11015eeb3736a203ec3a0ffd2edea744
AnshenBNY <1249890854@qq.com>
2026-06-04 10:38:02 +0800

    0604

--- Assets/GameFramework/Scripts/Character/ItemDefinition.cs
+++ Assets/GameFramework/Scripts/Character/ItemDefinition.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-namespace GameFramework.Character
+namespace GameFramework.Definitions
 {
     /// <summary>
     /// 道具定义：当前提供最常见的“瞬时回血”基础能力。
     /// 后续可扩展成“护甲包/弹药包/临时增益”等道具。
     /// </summary>
-    [CreateAssetMenu(menuName = "GameFramework/Character/Item Definition", fileName = "ItemDefinition")]
+    [CreateAssetMenu(menuName = "GameFramework/Definitions/Item Definition", fileName = "ItemDefinition")]
     public class ItemDefinition : ScriptableObject
     {
         public string itemId = "item_medkit_01";
